ac2deff92eb3284a3d608635d8dddb4252079cc5
zhaowh3613 <zhaowh3613@outlook.com>
2017-01-09 01:27:31 +0800

    post to database

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,15 @@
 exports.render = function(req, res){
-  if (req.session.lastVisit) {
-    console.log(req.session.lastVisit);
-  }
+  // if (req.session.lastVisit) {
+  //   console.log(req.session.lastVisit);
+  // }
 
-  req.session.lastVisit = new Date();
+  // req.session.lastVisit = new Date();
 
-  res.render('index', {
-    title: 'meanapp' + req.session.lastVisit
+  // res.render('index', {
+  //   title: 'meanapp' + req.session.lastVisit
+  // });
+  res.render('index',{
+    title: 'hello sign successful',
+    userFullName: req.users ? req.users.fullName: ''
   });
 };
--- config/express.js
+++ config/express.js
@@ -4,10 +4,12 @@ var config = require('./config'),
   compress = require('compression'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
-  session = require('express-session');
+  session = require('express-session'),
+  flash = require('connect-flash'),
+  passport = require('passport');
 
-var uri = 'mongodb://localhost/mean-dev';
-var db = require('mongoose').connect(uri);
+// var uri = 'mongodb://localhost/mean-dev';
+// var db = require('mongoose').connect(uri);
 module.exports = function(){
     var app = express();
 
@@ -35,6 +37,9 @@ module.exports = function(){
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
     
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
     require('../app/routes/index.server.routes.js') (app);
     require('../app/routes/users.server.routes.js') (app);
 
