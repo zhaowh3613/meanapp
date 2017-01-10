5d1533a3070e2fe35c6c36591a6e2852b3f567eb
zhaowh3613 <zhaowh3613@outlook.com>
2017-01-11 00:22:17 +0800

    fix no db response

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 var config = require('./config'),
 mongoose = require('mongoose');
 module.exports = function(){
-    var db = mongoose.createConnection(config.db);
-    db.on('error', console.error.bind(console, 'db connection error:'));
-    db.once('open', function(callback){
-        console.log('db connect successful');
-    });
+    var db = mongoose.connect(config.db);
+    //db.on('error', console.error.bind(console, 'db connection error:'));
+    // db.once('open', function(callback){
+    //     console.log('db connect successful');
+    // });
     require('../app/models/users.server.model');
     return db;
 }
